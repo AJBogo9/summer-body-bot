f1289d9bc6023a48bee10377ef9d86116a1f572f
Eppu <eppu.ruotsalainen@aalto.fi>
2025-03-06 15:33:28 +0200

    Particularize some instructions

--- flows/information-flows/help.js
+++ flows/information-flows/help.js
@@ -6,12 +6,12 @@ helpScene.enter(async (ctx) => {
 
   if (ctx.update.message.chat.type === 'private') {
     helpMessage += '/start \\- Get introduction & instructions to get basic information about the competition\\.\n\n'
-    helpMessage += '/register \\- Initiate your registration, create or join a team, and set up your profile\\.\n\n'
+    helpMessage += '/register \\- Initiate your registration and set up your profile\\.\n\n'
     helpMessage += '/createteam \\- Form a new team\\.\n\n'
     helpMessage += '/jointeam \\- Join an existing team\\. You will need the team ID, provided to who created the team\\.\n\n'
     helpMessage += '/addexercise \\- Log your Kilometer\\-based and Hour\\-based training\\.\n\n'
     helpMessage += '/weekscores \\- Log your weekly achievements to earn points every Sunday\\.\n\n'
-    helpMessage += '/howtogetpoints \\- Discover the various activities to earn points\\.\n\n'
+    helpMessage += '/howtogetpoints \\- Discover the various ways to earn points\\.\n\n'
     helpMessage += '/statsinfo \\- Show commands for getting rankings and stats\\.\n\n'
     helpMessage += '/terms \\- Read current terms and conditions\\.\n\n'
     helpMessage += 'If there is something that you did not understand or something problematic comes up you can send me a message on Telegram @EppuRuotsalainen\\.'
